52e75ef18be217a767baab3cc880f502b502759a
pooza <pooza@6bc21610-e001-0410-9273-fe8b54c00d2d>
2010-01-17 10:07:48 +0000

    git-svn-id: http://svn.b-shock.co.jp/commons/forms/trunk@3318 6bc21610-e001-0410-9273-fe8b54c00d2d

--- webapp/modules/UserForm/actions/ThanxAction.class.php
+++ webapp/modules/UserForm/actions/ThanxAction.class.php
@@ -10,6 +10,7 @@
 class ThanxAction extends BSRecordAction {
 	public function execute () {
 		$this->request->setAttribute('answer', $this->user->getAttribute('answer'));
+		$this->user->removeAttribute('answer')
 		return BSView::SUCCESS;
 	}
 }
