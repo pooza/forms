2763be82a52dfc409b5c460f50b322343d59bc3e
pooza <pooza@6bc21610-e001-0410-9273-fe8b54c00d2d>
2010-02-15 05:16:33 +0000

    git-svn-id: http://svn.b-shock.co.jp/commons/forms/trunk@3433 6bc21610-e001-0410-9273-fe8b54c00d2d

--- webapp/modules/UserForm/actions/RegisterAction.class.php
+++ webapp/modules/UserForm/actions/RegisterAction.class.php
@@ -52,6 +52,11 @@ class RegisterAction extends BSRecordAction {
 				}
 			}
 		}
+		foreach ($this->getRecord()->getFields() as $field) {
+			if ($field->isFile()) {
+				$field->clearTemporaryFile();
+			}
+		}
 		return BSView::INPUT;
 	}
 
