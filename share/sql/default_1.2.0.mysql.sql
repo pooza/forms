d65d726822f3c325dcb16b0c3d30dc4f63e8da00
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2011-02-01 16:11:52 +0900

    refs #2215

---
+++ share/sql/default_1.2.0.mysql.sql
@@ -0,0 +1,15 @@
+# 1.1.2→1.2.0へのアップグレード
+#
+# @package jp.co.commons.forms
+# @author 小石達也 <tkoishi@b-shock.co.jp>
+
+SET NAMES 'utf8';
+ALTER TABLE `form` ADD `pc_form_template` text NULL DEFAULT NULL  AFTER `email`;
+ALTER TABLE `form` ADD `pc_confirm_template` text NULL DEFAULT NULL  AFTER `pc_form_template`;
+ALTER TABLE `form` ADD `pc_thanx_template` text NULL DEFAULT NULL  AFTER `pc_confirm_template`;
+ALTER TABLE `form` ADD `mobile_form_template` text NULL DEFAULT NULL  AFTER `pc_thanx_template`;
+ALTER TABLE `form` ADD `mobile_confirm_template` text NULL DEFAULT NULL  AFTER `mobile_form_template`;
+ALTER TABLE `form` ADD `mobile_thanx_template` text NULL DEFAULT NULL  AFTER `mobile_confirm_template`;
+ALTER TABLE `form` ADD `smartphone_form_template` text NULL DEFAULT NULL  AFTER `mobile_thanx_template`;
+ALTER TABLE `form` ADD `smartphone_confirm_template` text NULL DEFAULT NULL  AFTER `smartphone_form_template`;
+ALTER TABLE `form` ADD `smartphone_thanx_template` text NULL DEFAULT NULL  AFTER `smartphone_confirm_template`;
