aaf7a68b4a03e2b2efef9acc41fd4ba6748819ff
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2011-02-01 16:05:22 +0900

    refs #2215

--- share/sql/default.mysql.sql
+++ share/sql/default.mysql.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.1.51, for apple-darwin10.4.0 (i386)
+-- MySQL dump 10.13  Distrib 5.1.54, for apple-darwin10.6.0 (i386)
 --
 -- Host: localhost    Database: forms
 -- ------------------------------------------------------
--- Server version	5.1.51
+-- Server version	5.1.54
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -85,6 +85,15 @@ CREATE TABLE `form` (
   `id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(32) NOT NULL,
   `email` varchar(64) DEFAULT NULL,
+  `pc_form_template` text,
+  `pc_confirm_template` text,
+  `pc_thanx_template` text,
+  `mobile_form_template` text,
+  `mobile_confirm_template` text,
+  `mobile_thanx_template` text,
+  `smartphone_form_template` text,
+  `smartphone_confirm_template` text,
+  `smartphone_thanx_template` text,
   `rank` smallint(5) unsigned DEFAULT NULL,
   `status` enum('show','hide') NOT NULL DEFAULT 'hide',
   `create_date` datetime NOT NULL,
@@ -191,4 +200,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2010-10-11 16:16:09
+-- Dump completed on 2011-02-01 16:04:39
