83b3a633ebf65684432671eabcaee0c90b463767
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2011-02-02 16:40:55 +0900

    fixes #2216

---
+++ webapp/modules/UserForm/UserFormModule.class.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * UserFormモジュール
+ *
+ * @package jp.co.commons.forms
+ * @subpackage UserForm
+ * @author 小石達也 <tkoishi@b-shock.co.jp>
+ * @version $Id$
+ */
+class UserFormModule extends BSModule {
+
+	/**
+	 * テンプレートを返す
+	 *
+	 * @access public
+	 * @param string $name テンプレート名
+	 * @return BSTemplateFile テンプレート
+	 */
+	public function getTemplate ($name) {
+		$record = $this->getRecord();
+		$useragent = $this->request->getUserAgent();
+
+		if ($useragent->isSmartPhone()) {
+			if (!BSString::isBlank($record['smartphone_' . $name . '_template'])) {
+				return $record->getTemplate('smartphone_' . $name);
+			}
+		}
+		if ($useragent->isSmartPhone() || $useragent->isMobile()) {
+			if (!BSString::isBlank($record['mobile_' . $name . '_template'])) {
+				return $record->getTemplate('mobile_' . $name);
+			}
+		}
+		return $record->getTemplate('pc_' . $name);
+	}
+}
+
+/* vim:set tabstop=4: */
