68a7d89e499a6a81894cde7d5ba16125b38891be
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2011-06-24 20:14:59 +0900

    carrot 2.4.96

--- webapp/modules/Default/actions/CookieDisabledAction.class.php
+++ webapp/modules/Default/actions/CookieDisabledAction.class.php
@@ -5,7 +5,6 @@
  * @package org.carrot-framework
  * @subpackage Default
  * @author 小石達也 <tkoishi@b-shock.co.jp>
- * @version $Id: CookieDisabledAction.class.php 1886 2010-02-28 04:45:35Z pooza $
  */
 class CookieDisabledAction extends BSAction {
 	public function execute () {
