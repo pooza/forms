44f21e3972227a9436d25461706d10bf66d29618
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2011-10-22 15:15:38 +0900

    carrot 2.4.137

--- webapp/lib/Registration/RegistrationHandler.class.php
+++ webapp/lib/Registration/RegistrationHandler.class.php
@@ -14,29 +14,6 @@ class RegistrationHandler extends BSTableHandler {
 	const OVERWRITE_CREATE_DATE = 512;
 
 	/**
-	 * レコード作成
-	 *
-	 * @access public
-	 * @param mixed $values 値
-	 * @param integer $flags フラグのビット列
-	 *   BSDatabase::WITHOUT_LOGGING ログを残さない
-	 * @return string レコードの主キー
-	 */
-	public function createRecord ($values, $flags = null) {
-		$value = new BSArray($values);
-		if (BSString::isBlank($values['user_agent'])) {
-			$values['user_agent'] = BSRequest::getInstance()->getUserAgent()->getName();
-		}
-		if (BSString::isBlank($values['remote_host'])) {
-			$values['remote_host'] = BSRequest::getInstance()->getHost()->getName();
-		}
-		if (BSString::isBlank($values['create_date'])) {
-			$values['create_date'] = BSDate::getNow('Y-m-d H:i:s');
-		}
-		return parent::createRecord($values, $flags);
-	}
-
-	/**
 	 * レコード追加可能か？
 	 *
 	 * @access protected
@@ -44,6 +21,26 @@ class RegistrationHandler extends BSTableHandler {
 	 */
 	protected function isInsertable () {
 		return true;
+	}
+
+	/**
+	 * ユーザーエージェントフィールド名
+	 *
+	 * @access public
+	 * @return string ユーザーエージェントフィールド名
+	 */
+	public function getUserAgentField () {
+		return 'user_agent';
+	}
+
+	/**
+	 * リモートホストフィールド名
+	 *
+	 * @access public
+	 * @return string リモートホストフィールド名
+	 */
+	public function getRemoteHostField () {
+		return 'remote_host';
 	}
 }
 
